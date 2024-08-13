85e70c6fbc25287ac3b38573df7e3418ec99e302
mywyau <47248104+mywyau@users.noreply.github.com>
2024-08-13 21:39:44 +0100

    add create post tests

--- src/test/scala/blog/configuration/ConfigSpec.scala
+++ src/test/scala/blog/configuration/ConfigSpec.scala
@@ -42,7 +42,7 @@ object ConfigSpec extends SimpleIOSuite {
     for {
       config <- configReader.loadChromedriverConfig
     } yield {
-      expect(config.chromedriver.options == List("--remote-allow-origins=*", "--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage")) &&
+      expect(config.chromedriver.options == List("--remote-allow-origins=*", "--disable-gpu", "--window-size=1920,1080", "--no-sandbox", "--disable-dev-shm-usage")) &&
         expect(config.chromedriver.path == PathConfig("webdriver.chrome.driver", "/usr/local/Caskroom/chromedriver/126.0.6478.126/chromedriver-mac-x64/chromedriver", "/home/runner/.nix-profile/bin/chromedriver"))
     }
   }
