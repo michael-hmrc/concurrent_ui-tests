c13f0088684ad5aa4faf8c0c0d0761a7ba311a37
mywyau <47248104+mywyau@users.noreply.github.com>
2024-08-15 15:55:19 +0100

    config

--- src/test/scala/blog/configuration/ConfigSpec.scala
+++ src/test/scala/blog/configuration/ConfigSpec.scala
@@ -43,7 +43,7 @@ object ConfigSpec extends SimpleIOSuite {
       config <- configReader.loadChromedriverConfig
     } yield {
       expect(config.chromedriver.options == List("--remote-allow-origins=*", "--disable-gpu", "--window-size=1920,1080", "--no-sandbox", "--disable-dev-shm-usage")) &&
-        expect(config.chromedriver.path == PathConfig("webdriver.chrome.driver", "/usr/local/Caskroom/chromedriver/126.0.6478.126/chromedriver-mac-x64/chromedriver", "/home/runner/.nix-profile/bin/chromedriver"))
+        expect(config.chromedriver.path == PathConfig("webdriver.chrome.driver", "/usr/local/Caskroom/chromedriver/126.0.6478.126/chromedriver-mac-x64/chromedriver", "/home/runner/.nix-profile/bin/chromedriver", "/usr/bin/chromedriver"))
     }
   }
 }
